aa3dd75ad93de8fc5e26c0433ff5a993a9f88d53
jnmrqs <jean.pessoal@pm.me>
2025-04-27 19:49:08 -0300

    1.5.6
    mais testes unitarios!

--- tests/test_math.c
+++ tests/test_math.c
@@ -2,14 +2,32 @@
 #include <assert.h>
 #include <stdio.h>
 
-int main(void)
+int main( void )
 {
-    assert(soma(1, 2) == 3);
-    assert(soma(-1, 1) == 0);
-    assert(diminui(1, 1) == 0);
-    assert(multiplica(3, 4) == 12);
-    assert(divide(2, 2) == 1);
+    assert( soma( 1, 2 ) == 3 );
+    assert( soma( 5, 5 ) == 10 );
+    assert( soma( -1, 2 ) == 1 );
+    assert( soma( 2, 2 ) == 4 );
+    assert( soma( 0, 0 ) == 0 );
 
-    printf("Todos os testes passaram!\n");
+    assert( diminui( 1, 1 ) == 0 );
+    assert( diminui( 0, 1 ) == -1 );
+    assert( diminui( 3, 2 ) == 1 );
+    assert( diminui( 20, 10 ) == 10 );
+    assert( diminui( 330, 330 ) == 0 );
+
+    assert( multiplica( 3, 4 ) == 12 );
+    assert( multiplica( 12, 12 ) == 144 );
+    assert( multiplica( -12, 10 ) == -120 );
+    assert( multiplica( 0, 0 ) == 0 );
+    assert( multiplica( 1, 1 ) == 1 );
+
+    assert( divide( 2, 2 ) == 1 );
+    assert( divide( 3, 2 ) == 1 );
+    assert( divide( 6, 3 ) == 2 );
+    assert( divide( 10, 2 ) == 5 );
+    assert( divide( 1, 1 ) == 1 );
+
+    printf( "Todos os testes passaram!\n" );
     return 0;
 }
